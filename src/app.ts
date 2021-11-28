6f38b950fe35558f94b9aad1a1aa554e4d97217a
ElianaValderrama <ecvalderrama@gmail.com>
2021-11-28 10:39:44 -0500

    commit de rutas

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,38 @@
 import express from 'express';
+import usuariosRoutes from './routes/usuarios';
 
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
-res.send('Prueba del servidor');
+//para imprimir el body como json
+app.use(express.json());
+app.use(express.urlencoded({ extended:true}));
+
+usuariosRoutes(app);
+
+app.get('/prueba/:id', async(req, res, next) => {
+    /*console.log(req.body)
+    //creacion promesa
+    console.log('antes de la promesa')
+    let x = 10;
+    const promesa = new Promise((resolve, reject) =>{
+    if(x == 10){
+        resolve('la promesa se resuelve')
+    }else{
+        reject('promesa rechazada')
+    }
+    });
+
+    //consumir la promesa
+    await promesa.then((respuesta) =>{
+        console.log(respuesta);
+    }).catch(err => {
+        console.log(err)
+    })
+
+    console.log('despues de la promesa')*/
+
+    res.status(404).json({message: "Todo Ok"});
 });
 
 app.listen(port, () => {
---
+++ src/routes/usuarios.ts
@@ -0,0 +1,15 @@
+import { Router } from "express"
+
+const usuariosRoutes = (app) => {
+    const router = Router();
+    app.use('/',router);
+    //obtiene todas los usuarios
+    router.get('/obtenerUsuarios', (req, res) => res.send('Obtener Canciones'));
+    //obtiene un solo usuario
+    router.get('/obtenerUsuarios/:id', (req, res) => res.send('Obtener Canciones'));
+    router.post('/agregarUsuarios', (req, res) => res.send('Obtener Canciones'));
+    router.put('/actualizarUsuarios', (req, res) => res.send('Obtener Canciones'));
+    router.delete('/eliminarUsuarios', (req, res) => res.send('Obtener Canciones'));
+}
+
+export default usuariosRoutes;
