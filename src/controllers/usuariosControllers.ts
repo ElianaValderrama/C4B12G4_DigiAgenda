570f7c308a86b9a7562e18ac134ad425d9061345
ElianaValderrama <ecvalderrama@gmail.com>
2021-11-28 20:24:25 -0500

    commit primer crud 2

--- src/controllers/usuariosControllers.ts
+++ src/controllers/usuariosControllers.ts
@@ -35,13 +35,13 @@ const actualizarUsuarios = (req, res) => {
     const {nombre_usuario, apellido_usuario, edad_usuario, cel_usuario, correo_usuario, genero_usuario, fecha_nac_usuario, password} = req.body;
     const {id} = req.params;
     executeQuery(`UPDATE usuarios SET nombre_usuario = '${nombre_usuario}', apellido_usuario = '${apellido_usuario}', edad_usuario = ${edad_usuario}, cel_usuario = ${cel_usuario}, correo_usuario = '${correo_usuario}', genero_usuario = '${genero_usuario}', fecha_nac_usuario = '${fecha_nac_usuario}', password = '${password}' WHERE id_usuarios = '${id}'`).then((response) => {
-        res.json({message: 'updates'});
+        res.json({message: response.affectedRows > 0 ? 'updated' : 'no existe registor con id: ${req.params.id}'});
     })
 }
 
 const eliminarUsuarios = (req, res) => {
     executeQuery(`DELETE FROM usuarios WHERE id_usuarios = '${req.params.id}'`).then((response) => {
-        res.json({message: 'deleted'});
+        res.json({message: response.affectedRows > 0 ? 'deleted' : 'no existe registor con id: ${req.params.id}'});
     }).catch((error) => {
         console.log(error);
         response.status(500).send(error);
